2ccc0caa3db84a38dc4e85c274d9a73e31915b42
Monochrome Chameleon <github@hughgreenish.com>
2016-10-01 22:26:38 +0100

    Marginal performance tweak, add test to verify instance independence

--- test/Whitelist.js
+++ test/Whitelist.js
@@ -48,7 +48,8 @@ var specs = [
     func: function () {
       return wl('foo').required
     },
-    required: true
+    required: true,
+    mixed: true
   }
 ]
 
@@ -119,6 +120,38 @@ specs.forEach(function (spec) {
           assert(request.wl.foo === 'foo')
           assert(request.wl.bar === 'bar')
         })
+      })
+    })
+
+    it('Should return a separate instance each time', function () {
+      var first = spec.func()('foo', 'bar')
+      var second = spec.func()('baz', 'bam')
+
+      assert(first !== second)
+      var firstRequest = { headers: {}, query: { foo: 'foo', bar: 'bar', baz: 'baz', bam: 'bam' } }
+      var secondRequest = { headers: {}, query: { foo: 'foo', bar: 'bar', baz: 'baz', bam: 'bam' } }
+
+      first(firstRequest, undefined, function () {
+        assert(firstRequest.hasOwnProperty('wl'))
+        assert(firstRequest.wl.hasOwnProperty('foo'))
+        assert(firstRequest.wl.hasOwnProperty('bar'))
+        assert(!firstRequest.wl.hasOwnProperty('baz'))
+        assert(!firstRequest.wl.hasOwnProperty('bam'))
+
+        assert(firstRequest.wl.foo === 'foo')
+        assert(firstRequest.wl.bar === 'bar')
+      })
+
+      second(secondRequest, undefined, function () {
+        assert(secondRequest.hasOwnProperty('wl'))
+        // The mixed spec will always whitelist foo
+        assert(secondRequest.wl.hasOwnProperty('foo') === !!spec.mixed)
+        assert(!secondRequest.wl.hasOwnProperty('bar'))
+        assert(secondRequest.wl.hasOwnProperty('baz'))
+        assert(secondRequest.wl.hasOwnProperty('bam'))
+
+        assert(secondRequest.wl.baz === 'baz')
+        assert(secondRequest.wl.bam === 'bam')
       })
     })
   })
